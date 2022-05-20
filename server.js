7d0a57325c0d1d65b136e42676b2947d063b81f1
Aman <aman2@me.iitr.ac.in>
2022-05-20 19:02:24 +0530

    :zap: add static files
    Signed-off-by: Aman <aman2@me.iitr.ac.in>

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import express from "express";
 import { APP_PORT } from "./config";
+import bodyParser from "body-parser";
 
 
 const app = express();
 
+// Getting data in json format
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+);
+app.use(bodyParser.json({ type: "application/json" }));
+
+// Setting express engine
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 // Starting the server
 app.listen(APP_PORT || 3000, () => {
